70ad1c2111394f04553b00013920ee9f14fa7556
Sari <sari@Inbars-Air.public.ph.local>
2020-02-03 16:21:45 +0100

    Create portfolio repository

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Paris',
+  shortName: 'Paris',
+  description: 'A colorful cheerful stylish path to design ',
+  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'SariKatariina',
+  twitter: 'Imed_Adel'
+}
